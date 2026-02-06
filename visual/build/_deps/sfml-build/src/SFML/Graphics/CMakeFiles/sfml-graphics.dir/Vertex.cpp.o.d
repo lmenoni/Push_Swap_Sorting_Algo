a684f675230147f41f5916723779953b71d01cb3
lmenoni <mejomeno@gmail.com>
2026-02-06 17:46:12 +0100

    Aggiungi visualizer al gitignore - contiene repo annidati

---
+++ visual/build/_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Vertex.cpp.o.d
@@ -0,0 +1,8 @@
+_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Vertex.cpp.o: \
+  /home/lmenoni/00CommonCore/push_swap/push_swap_visualizer/build/_deps/sfml-src/src/SFML/Graphics/Vertex.cpp \
+  /home/lmenoni/00CommonCore/push_swap/push_swap_visualizer/build/_deps/sfml-src/include/SFML/Graphics/Vertex.hpp \
+  /home/lmenoni/00CommonCore/push_swap/push_swap_visualizer/build/_deps/sfml-src/include/SFML/Graphics/Export.hpp \
+  /home/lmenoni/00CommonCore/push_swap/push_swap_visualizer/build/_deps/sfml-src/include/SFML/Config.hpp \
+  /home/lmenoni/00CommonCore/push_swap/push_swap_visualizer/build/_deps/sfml-src/include/SFML/Graphics/Color.hpp \
+  /home/lmenoni/00CommonCore/push_swap/push_swap_visualizer/build/_deps/sfml-src/include/SFML/System/Vector2.hpp \
+  /home/lmenoni/00CommonCore/push_swap/push_swap_visualizer/build/_deps/sfml-src/include/SFML/System/Vector2.inl
